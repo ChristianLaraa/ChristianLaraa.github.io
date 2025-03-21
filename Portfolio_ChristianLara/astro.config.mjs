5a97698bb2f8a442e6a8a55c989145574fab84b5
Christian Lara <christian.lara.091204@gmail.com>
2025-03-20 21:56:03 -0600

    more advance in proyectos, curriculum, about and add navbar, icons

--- Portfolio_ChristianLara/astro.config.mjs
+++ Portfolio_ChristianLara/astro.config.mjs
@@ -2,19 +2,24 @@
 import { defineConfig } from 'astro/config';
 import tailwindcss from "@tailwindcss/vite";
 
+import icon from 'astro-icon';
+
 // https://astro.build/config
 export default defineConfig({
-    output: 'static',
-    vite: {
-        ssr:{
-            noExternal: ['gsap']
-        },
-        build:{
-            target: 'esnext'
-        },
-        optimizeDeps: {
-            include: ['gsap'],
-        },
-        plugins: [tailwindcss()],
-    },
+  output: 'static',
+
+  vite: {
+      ssr:{
+          noExternal: ['gsap']
+      },
+      build:{
+          target: 'esnext'
+      },
+      optimizeDeps: {
+          include: ['gsap'],
+      },
+      plugins: [tailwindcss()],
+  },
+
+  integrations: [icon()],
 });
--- Portfolio_ChristianLara/src/assets/data.js
+++ Portfolio_ChristianLara/src/assets/data.js
@@ -2,55 +2,12 @@ export const socials = [{ logo: 'bx bxl-youtube' }, { logo: 'bx bxl-linkedin-squ
 
 export const info = [{ text: 'About' }, { text: 'Projects' }];
 
-export const technologies = [
-   {
-      text: 'TypeScript',
-      logo: 'bx bxl-typescript',
-   },
-   {
-      text: 'JavaScript',
-      logo: 'bx bxl-javascript',
-   },
-   {
-      text: 'PostgreSQL',
-      logo: 'bx bxl-postgresql',
-   },
-   {
-      text: 'AWS',
-      logo: 'bx bxl-aws',
-   },
-   {
-      text: 'Docker',
-      logo: 'bx bxl-docker',
-   },
-   {
-      text: 'CSS',
-      logo: 'bx bxl-css3',
-   },
-   {
-      text: 'Python',
-      logo: 'bx bxl-python',
-   },
-   {
-      text: 'React',
-      logo: 'bx bxl-react',
-   },
-   {
-      text: 'HTML',
-      logo: 'bx bxl-html5',
-   },
-   {
-      text: 'GitHub',
-      logo: 'bx bxl-github',
-   },
-];
-
 export const projects = [
    {
-      title: 'Blog',
+      title: 'Web1',
       description:
          'Lorem ipsum dolor sit amet consectetur adipisicing elit. Placeat voluptatem ad, exercitationem in beatae voluptate fugit hic error, et quod maxime repellat iusto! Iusto nisi soluta hic minima! Temporibus sequi incidunt vitae dolorum error illo ducimus saepe blanditiis pariatur. Optio amet cum excepturi dignissimos aut tenetur repudiandae ad dolorem, distinctio minima inventore, neque adipisci, facilis autem quis quod nemo. Delectus sint deserunt repellat ea odio cupiditate repellendus eius saepe ipsa dicta vel nulla ullam sit deleniti accusantium quas dolore, veritatis dolores totam vitae voluptatem placeat fugiat similique alias? Possimus numquam aut illo velit repr.',
-      image: 'image-blog',
+      image: 'image-web1',
       color: '#f3c201',
    },
    {

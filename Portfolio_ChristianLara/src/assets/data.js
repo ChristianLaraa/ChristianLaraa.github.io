f287971e2907822da1bbe8f685748e9adc388b2a
Christian Lara <christian.lara.091204@gmail.com>
2025-03-21 19:07:21 -0600

    add curriculum and more components

--- Portfolio_ChristianLara/src/assets/data.js
+++ Portfolio_ChristianLara/src/assets/data.js
@@ -1,4 +1,4 @@
-export const socials = [{ logo: 'bx bxl-youtube' }, { logo: 'bx bxl-linkedin-square' }, { logo: 'bx bxl-instagram' }, { logo: 'bx bxl-facebook-circle' }, { logo: 'bx bxl-github' }];
+export const socials = [{ logo: 'bx bxl-linkedin-square' }, { logo: 'bx bxl-instagram' }, { logo: 'bx bxl-github' }];
 
 export const info = [{ text: 'About' }, { text: 'Projects' }];
 

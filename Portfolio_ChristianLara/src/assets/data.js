fc8370c34da6e3046686ee24bf29ccf7f78f0026
Christian Lara <christian.lara.091204@gmail.com>
2025-03-26 20:13:00 -0600

    correction in contact

--- Portfolio_ChristianLara/src/assets/data.js
+++ Portfolio_ChristianLara/src/assets/data.js
@@ -1,4 +1,4 @@
-export const socials = [{ logo: 'bx bxl-linkedin-square' }, { logo: 'bx bxl-instagram' }, { logo: 'bx bxl-github' }];
+export const socials = [{ logo: 'bx bxl-linkedin-square' }, { logo: 'bx bxl-github' }];
 
 export const info = [{ text: 'About' }, { text: 'Projects' }];
 export const technologies = [
